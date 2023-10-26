2ecbffcfc8bcce84acd468600f06cc00191e578f
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2023-10-26 20:40:55 +0200

    fix(index.blade.php): remove [*.] & .blade.php from matched links, so we don't generate like /projects/game/[name]

--- resources/views/pages/projects/index.blade.php
+++ resources/views/pages/projects/index.blade.php
@@ -18,7 +18,7 @@ render(function (View $view) {
 
     $projects = collect($views)->map(
         static function (SplFileInfo $splFileInfo) {
-            $path = str_replace('.blade.php', '', $splFileInfo->getRelativePathname());
+            $path = preg_replace('/\[[^\]]*\]|\.blade\.php/', '', $splFileInfo->getRelativePathname());
 
             return [
                 'href' => url("/projects/$path"),
@@ -52,7 +52,7 @@ render(function (View $view) {
         <ul role="list"
             class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             @foreach($projects as $project)
-                <a href="{{ $project['href'] }}" wire:navigate>
+                <a href="{{ $project['href'] }}">
                     <img class="aspect-[3/2] w-full rounded-2xl object-cover"
                          src="{{ Storage::url('projects/tyler-van-der-hoeven-_ok8uVzL2gI-unsplash.jpg') }}"
                          alt="">
