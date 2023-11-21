2b518d7770a3767416249cdd93bbe09b9927d76d
Sasin91 <jonas.kerwin.hansen@gmail.com>
2023-11-21 17:49:28 +0100

    fix auth logout test

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -35,7 +35,7 @@ test('users can not authenticate with invalid password', function () {
 test('users can logout', function () {
     $user = User::factory()->create();
 
-    $response = $this->actingAs($user)->post('/logout');
+    $response = $this->actingAs($user)->get('/logout');
 
     $this->assertGuest();
     $response->assertRedirect('/');
