7aa5543652e9bba10abd09825f4f0342791069a0
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2024-03-15 07:08:20 +0100

    added href to <Logo /> & refactored to using obj. destructuring

--- resources/js/Components/ui/Logo.tsx
+++ resources/js/Components/ui/Logo.tsx
@@ -9,25 +9,36 @@ import { byRadius } from "@cloudinary/url-gen/actions/roundCorners";
 
 import { cloudinaryAppLogo } from "@/config";
 import cld from "@/utils/cloudinary";
+import { Link } from "@inertiajs/react";
 
-export default function Logo(props: {
+export default function Logo({
+    width,
+    height,
+    radius,
+    className,
+    href = '/',
+    ...rest
+}: {
     width?: number;
     height?: number;
     radius?: number;
     className?: string;
+    href?: string;
 }) {
     const logo = cld.image(cloudinaryAppLogo);
     logo.resize(
         fill()
-            .width(props.width ?? 64)
-            .height(props.height ?? 64)
-    ).roundCorners(byRadius(props.radius ?? 25));
+            .width(width ?? 64)
+            .height(height ?? 64)
+    ).roundCorners(byRadius(radius ?? 25));
 
     return (
-        <AdvancedImage
-            cldImg={logo}
-            plugins={[responsive(), placeholder(), lazyload()]}
-            {...props}
-        />
+        <Link href={href}>
+            <AdvancedImage
+                cldImg={logo}
+                plugins={[responsive(), placeholder(), lazyload()]}
+                {...rest}
+            />
+        </Link>
     );
 }
