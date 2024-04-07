90687c7e0dab78a8829fbcf229dad289eb9d2210
sasin91 <jonas.kerwin.hansen@gmail.com>
2024-04-07 17:25:05 +0200

    Updated input.tsx to use latest shadcn

--- resources/js/Components/ui/Input.tsx
+++ resources/js/Components/ui/Input.tsx
@@ -1,52 +1,24 @@
-import { cn } from "@/utils/tailwind";
-import {
-    InputHTMLAttributes,
-    forwardRef,
-    useEffect,
-    useImperativeHandle,
-    useRef,
-} from "react";
+import { cn } from "@/utils/tailwind"
+import * as React from "react"
 
-export type InputProps = InputHTMLAttributes<HTMLInputElement> & {
-    isFocused?: boolean;
-};
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
-    ({ className, type, isFocused = false, ...props }: InputProps, ref) => {
-        const localRef = useRef<HTMLInputElement>(null);
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = "Input"
 
-        useImperativeHandle<Partial<typeof ref>, Partial<typeof ref>>(
-            ref,
-            () => ({
-                focus: () => localRef.current?.focus(),
-            })
-        );
-
-        useEffect(() => {
-            if (isFocused) {
-                localRef.current?.focus();
-            }
-        }, []);
-
-        return (
-            <input
-                type={type}
-                className={cn(
-                    "flex w-full h-10 px-3 py-2 text-sm",
-                    "border border-gray-300 rounded-md",
-                    "focus:border-indigo-500 focus:ring-indigo-500 focus-visible:ring-indigo-500",
-                    "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
-                    "shadow-sm placeholder:text-muted-foreground",
-                    "file:border-0 file:bg-transparent file:text-sm file:font-medium",
-                    "disabled:cursor-not-allowed disabled:opacity-50",
-                    className
-                )}
-                ref={ref}
-                {...props}
-            />
-        );
-    }
-);
-Input.displayName = "Input";
-
-export { Input };
+export { Input }
