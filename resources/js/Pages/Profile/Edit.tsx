d470a266ad6ea1d4a8e03c2de93c43a14e3ad43e
sasin91 <jonas.kerwin.hansen@gmail.com>
2024-04-07 19:11:12 +0200

    Switch authenticatedLayout for AppLayout

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -1,17 +1,24 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import DeleteUserForm from './Partials/DeleteUserForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
 import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
+import AppLayout from '@/Layouts/AppLayout';
+import Headline from '@/Components/ui/Headline';
+import { useTranslation } from '@/i18n/client';
 
-export default function Edit({ auth, mustVerifyEmail, status }: PageProps<{ mustVerifyEmail: boolean, status?: string }>) {
+export default function Edit({ mustVerifyEmail, status }: PageProps<{ mustVerifyEmail: boolean, status?: string }>) {
+    const { t } = useTranslation();
+
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Profile</h2>}
-        >
-            <Head title="Profile" />
+        <AppLayout>
+            <Head title={t('navigation.authenticated.profile')} />
+
+            <header className="bg-background shadow">
+                <div className="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <Headline>{t('navigation.authenticated.profile')}</Headline>
+                </div>
+            </header>
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
@@ -32,6 +39,6 @@ export default function Edit({ auth, mustVerifyEmail, status }: PageProps<{ must
                     </div>
                 </div>
             </div>
-        </AuthenticatedLayout>
+        </AppLayout>
     );
 }
